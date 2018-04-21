9de131770a9f74c7a592d0fec7f696e90320eef3
Simon Reich <simon@laptop>
2018-04-21 15:26:41 +0200

    Timer moved into class. Split screen for second timer added

--- timer.py
+++ timer.py
@@ -20,10 +20,7 @@ This file is part of 100x100m Timer.
 
 
 # Time before counter starts in seconds
-timePrecounter = 30
-
-# Time for one lap in seconds
-timeLap = 120
+timePrecounter = 3
 
 # Number of laps
 nrLap = 100
@@ -31,13 +28,10 @@ nrLap = 100
 # Break before this laps
 # For example nrBreak = [10, 20], means that a break will be inserted between lap 9-10 and lap lap 19-20
 # Leave empty for no breaks nrBreak = []
-nrBreak = [26, 51, 76]
+nrBreak = [31, 51, 81]
 
 # Length of break in seconds
 timeBreak = 300
-
-# Pitch of start tunes
-tunePitch = [440, 440, 440]
 
 
 
@@ -50,15 +44,7 @@ tunePitch = [440, 440, 440]
 import pygame
 from pygame.mixer import Sound, get_init, pre_init
 from array import array
-
-
-pre_init(44100, -16, 1, 1024)
-pygame.init()
-screen = pygame.display.set_mode((1920, 1020))
-clock = pygame.time.Clock()
-
-pygame.time.set_timer(pygame.USEREVENT, 1000)
-font = pygame.font.SysFont('Consolas', 600)
+import sys
 
 
 
@@ -84,119 +70,183 @@ class Note(Sound):
         return samples
 
 
+class Timer():
+    def __init__(self, _timePrecounter, _timeLap, _nrLap, _nrBreak, _timeBreak):
+        # Time before counter starts in seconds
+        self.timePrecounter = _timePrecounter
 
-# The window always shows two texts:
-textTop = ""
-textBottom = ""
+        # Time for one lap in seconds
+        self.timeLap = _timeLap
 
-# Phase of the program
-# phase = 1 Countdown before Timer
-# phase = 2 Timer, running nrLap times
-# phase = 3 Swimming break
-# phase = 4 Done 
-phase = 1
+        # Number of laps
+        self.nrLap = _nrLap
 
-# Countdown
-cTimePrecounter = timePrecounter
-m, s = divmod(timePrecounter, 60)
+        # Break before this laps
+        # For example nrBreak = [10, 20], means that a break will be inserted between lap 9-10 and lap lap 19-20
+        # Leave empty for no breaks nrBreak = []
+        self.nrBreak = _nrBreak
 
-# Timer
-cTimeLap = timeLap
-cNrLap = 1
+        # Length of break in seconds
+        self.timeBreak = _timeBreak
 
-# Break
-cTimeBreak = timeBreak
-cNrBreak = nrBreak
+        # The window always shows two texts:
+        self.textTop = ""
+        self.textBottom = ""
+
+        # Phase of the program
+        # phase = 1 Countdown before Timer
+        # phase = 2 Timer, running nrLap times
+        # phase = 3 Swimming break
+        # phase = 4 Done 
+        self.phase = 1
+
+        # Countdown
+        self.cTimePrecounter = self.timePrecounter
+        self.m, self.s = divmod(self.timePrecounter, 60)
+
+        # Timer
+        self.cTimeLap = self.timeLap
+        self.cNrLap = 1
+
+        # Break
+        self.cTimeBreak = self.timeBreak
+        self.cNrBreak = self.nrBreak
+
+        # Sound
+        self.sound = "on"
+
+        # Pitch of start tunes
+        self.tunePitch = [440, 440, 440]
+
+    def update(self):
+        # Phase 1 is Countdown before timer
+        if self.phase == 1:
+            # Substract Counter and update text
+            self.cTimePrecounter -= 1
+            self.m, self.s = divmod(self.cTimePrecounter, 60)
+            self.textTop = "Los!"
+            self.textBottom = str(self.m).rjust(2) + ":" + str(self.s).zfill(2) + " m"
+
+            # Play sounds
+            if self.cTimePrecounter == 2:
+                Note(self.tunePitch[0]).play(100)
+            if self.cTimePrecounter == 1:
+                Note(self.tunePitch[1]).play(100)
+            if self.cTimePrecounter <= 0:
+                Note(self.tunePitch[2]).play(100)
+                self.phase = 2
+
+        # Phase 2 is Timer
+        if self.phase == 2:
+            # Substract Counter and update text
+            self.cTimeLap -= 1
+            self.m, self.s = divmod(self.cTimeLap, 60)
+            self.textTop = "# " + str(self.cNrLap).ljust(3)
+            self.textBottom = str(self.m).rjust(1) + ":" + str(self.s).zfill(2) + " m"
+
+            # Play sounds
+            if self.cTimeLap == 2 and self.sound == "on":
+                Note(self.tunePitch[0]).play(100)
+            if self.cTimeLap == 1 and self.sound == "on":
+                Note(self.tunePitch[1]).play(100)
+            if self.cTimeLap <= 0 and self.sound == "on":
+                Note(self.tunePitch[2]).play(100)
+
+            # Is lap finished
+            if self.cTimeLap <= 0:
+                self.cNrLap += 1
+                self.cTimeLap = self.timeLap
+
+            # Is a break comming up
+            if self.cNrLap in self.nrBreak:
+                self.cTimeBreak = self.timeBreak
+                self.cNrBreak.remove(self.cNrLap)
+                self.phase = 3
+
+            # Is series finished
+            if self.cNrLap > self.nrLap:
+                self.self.phase= 4
+
+            # do not play sounds before break or end
+            if self.cNrLap+1 in self.nrBreak or self.cNrLap+1 > self.nrLap:
+                self.sound = "off"
+            else:
+                self.sound = "on"
+
+        # Phase 3 is break
+        if self.phase == 3:
+            # Substract Counter and update text
+            self.cTimeBreak -= 1
+            self.m, self.s = divmod(self.cTimeBreak, 60)
+            self.textTop = "Pause"
+            self.textBottom = str(self.m).rjust(1) + ":" + str(self.s).zfill(2) + " m"
+
+            # Play sounds
+            if self.cTimeBreak == 2:
+                Note(self.tunePitch[0]).play(100)
+            if self.cTimeBreak == 1:
+                Note(self.tunePitch[1]).play(100)
+            if self.cTimeBreak <= 0:
+                Note(self.tunePitch[2]).play(100)
+                self.cTimeLap = self.timeLap
+                self.phase = 2
+
+        # Phase 4 is done
+        if self.phase == 4:
+            self.textTop = "Lap: " + str(self.cNrLap).ljust(3) + " Fertig"
+            self.textBottom = "Geschafft!"
+
+    def getText(self):
+        return self.textTop, self.textBottom
+
+def main(argv=None):
+    """ main function
+        argv: possible arguments, will be replaced by sys.argv if none are 
+              given.
+    """
+    if argv is None:
+        argv = sys.argv
+
+    pre_init(44100, -16, 1, 1024)
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    clock = pygame.time.Clock()
+
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+    font = pygame.font.SysFont('Consolas', 140)
 
 
 
-# This is the main loop
-while True:
-    for e in pygame.event.get():
-        if e.type == pygame.USEREVENT: 
-            # Phase 1 is Countdown before timer
-            if phase == 1:
-                # Substract Counter and update text
-                cTimePrecounter -= 1
-                m, s = divmod(cTimePrecounter, 60)
-                textTop = "Los!"
-                textBottom = str(m).rjust(2) + ":" + str(s).rjust(2) + " m"
+    # Class 1: 120s
+    timer120 = Timer(timePrecounter, 5, nrLap, nrBreak, timeBreak)
 
-                # Play sounds
-                if cTimePrecounter == 2:
-                    Note(tunePitch[0]).play(100)
-                if cTimePrecounter == 1:
-                    Note(tunePitch[1]).play(100)
-                if cTimePrecounter <= 0:
-                    Note(tunePitch[2]).play(100)
-                    phase = 2
+    # Class 2: 135s
+    timer135 = Timer(timePrecounter, 7, nrLap, nrBreak, timeBreak)
 
-            # Phase 2 is Timer
-            if phase == 2:
-                # Substract Counter and update text
-                cTimeLap -= 1
-                m, s = divmod(cTimeLap, 60)
-                textTop = "Lap: " + str(cNrLap).ljust(3)
-                textBottom = str(m).rjust(1) + ":" + str(s).ljust(2) + " m"
+    # This is the main loop
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.USEREVENT: 
+                timer120.update()
+                timer135.update()
+            if e.type == pygame.QUIT: break
+        else:
+            textTop120, textBottom120 = timer120.getText()
+            textTop135, textBottom135 = timer135.getText()
 
-                # Play sounds
-                if cTimeLap == 2 and sound == "on":
-                    Note(tunePitch[0]).play(100)
-                if cTimeLap == 1 and sound == "on":
-                    Note(tunePitch[1]).play(100)
-                if cTimeLap <= 0 and sound == "on":
-                    Note(tunePitch[2]).play(100)
+            screen.fill((255, 255, 255))
+            yoffset = 50
+            xoffset = 300
+            screen.blit(font.render(textTop120, True, (0, 0, 0)), (10, yoffset))
+            screen.blit(font.render(textBottom120, True, (0, 0, 0)), (300, yoffset))
 
-                # Is lap finished
-                if cTimeLap <= 0:
-                    cNrLap += 1
-                    cTimeLap = timeLap
+            screen.blit(font.render(textTop135, True, (0, 0, 0)), (10, (480/2)+yoffset))
+            screen.blit(font.render(textBottom135, True, (0, 0, 0)), (300, (480/2)+yoffset))
+            pygame.display.flip()
+            clock.tick(60)
+            continue
+        break
 
-                # Is a break comming up
-                if cNrLap in nrBreak:
-                    cTimeBreak = timeBreak
-                    cNrBreak.remove(cNrLap)
-                    phase = 3
 
-                # Is series finished
-                if cNrLap > nrLap:
-                    phase= 4
-
-                # do not play sounds before break or end
-                if cNrLap+1 in nrBreak or cNrLap+1 > nrLap:
-                    sound = "off"
-                else:
-                    sound = "on"
-
-            # Phase 3 is break
-            if phase == 3:
-                # Substract Counter and update text
-                cTimeBreak -= 1
-                m, s = divmod(cTimeBreak, 60)
-                textTop = "Pause"
-                textBottom = str(m).rjust(1) + ":" + str(s).ljust(2) + " m"
-
-                # Play sounds
-                if cTimeBreak == 2:
-                    Note(tunePitch[0]).play(100)
-                if cTimeBreak == 1:
-                    Note(tunePitch[1]).play(100)
-                if cTimeBreak <= 0:
-                    Note(tunePitch[2]).play(100)
-                    cTimeLap = timeLap
-                    phase = 2
-
-            # Phase 4 is done
-            if phase == 4:
-                textTop = "Lap: " + str(cNrLap).ljust(3) + " Fertig"
-                textBottom = "Herzlichen Glückwunsch!"
-        if e.type == pygame.QUIT: break
-    else:
-        screen.fill((255, 255, 255))
-        screen.blit(font.render(textTop, True, (0, 0, 0)), (120, 24))
-        screen.blit(font.render(textBottom, True, (0, 0, 0)), (32, 500))
-        pygame.display.flip()
-        clock.tick(60)
-        continue
-    break
-
+if __name__ == "__main__":
+    main()
